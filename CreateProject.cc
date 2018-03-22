7cf6b3c258e1752b1df4b3a7d4df41618847a250
KrzysztofSkotnicki <krzysztof@skotnicki.it>
2018-03-22 13:09:19 +0100

    createProject

---
+++ CreateProject.cc
@@ -0,0 +1,4 @@
+admCreateProject(customfield_12133, customfield_12132, "Three thunderbolts in a single hole", customfield_12134, "http://link.pl", "PUBLIC", false, 0, "software");
+lfDialogMessage("Projekt dodany " + customfield_12132, "INFO");
+
+//admCreateProject(pkey, pname, description, lead, url, categoryName, defaultIsUnassigned, avatarId, projectTypeKey)
